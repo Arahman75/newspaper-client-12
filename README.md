53a17ae944766259d56a3dbbaf60a54b53de6015
Arahman75 <selfarahman01825@gmail.com>
2023-11-29 14:58:48 +0600

    added the my profile page

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## live link: https://newspaper-3507b.web.app
 
 ## https://github.com/programming-hero-web-course1/b8a12-client-side-Arahman75
+
+## Assignment Category: assignment12_category_0012
--- src/pages/MyProfile/MyProfile.jsx
+++ src/pages/MyProfile/MyProfile.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../provide/AuthProvider';
 
 const MyProfile = () => {
+    const { user } = useContext(AuthContext);
+    console.log(user);
     return (
-        <div>
-            <h3>This is my profile</h3>
+        <div className='w-[300px] mx-auto text-center'>
+            <img className='w-full' src={user?.photoURL} alt="user photo" />
+            <h2 className='text-2xl font-bold py-4'>{user?.displayName}</h2>
+            <h3 className='text-xl font-semibold'>{user?.email}</h3>
         </div>
     );
 };
